6f40a2b1fe34d404f3a7ee829055a5b252089e7a
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-12-17 11:12:54 -0800

    "Add blank lines in CategoryRepository constructor for readability
    Improved code readability by adding blank lines in the constructor of the CategoryRepository class. This change follows standard formatting practices, ensuring better code organization and clarity."

--- src/AspireBlog.Data.Mongo/Implementation/CategoryRepository.cs
+++ src/AspireBlog.Data.Mongo/Implementation/CategoryRepository.cs
@@ -11,10 +11,14 @@ namespace AspireBlog.Data.Mongo.Implementation;
 
 public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
 {
+
 	private readonly BlogDbContext _context;
 
 	public CategoryRepository(BlogDbContext context) : base(context)
 	{
+
 		_context = Guard.Against.Null(context, nameof(context));
+
 	}
+
 }
