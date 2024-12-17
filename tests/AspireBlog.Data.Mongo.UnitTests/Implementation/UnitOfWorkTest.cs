aed4c7a8a040cc46fd29ed1fcb6b7a1c0de20c54
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-12-17 10:57:03 -0800

    Add skeleton for UnitOfWorkTest in Mongo unit tests
    Created a new test class `UnitOfWorkTest` with a basic structure and placeholder method. Actual implementation of test cases will be added in future commits.

---
+++ tests/AspireBlog.Data.Mongo.UnitTests/Implementation/UnitOfWorkTest.cs
@@ -0,0 +1,31 @@
+// ============================================
+// Copyright (c) 2024. All rights reserved.
+// File Name :     UnitOfWorkTest.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : AspireBlog
+// Project Name :  AspireBlog.Data.Mongo.UnitTests
+// =============================================
+
+namespace AspireBlog.Data.Mongo.Implementation;
+
+[TestSubject(typeof(UnitOfWork))]
+[ExcludeFromCodeCoverage]
+public class UnitOfWorkTest
+{
+
+	[Fact]
+	public void METHOD()
+	{
+
+		//Arrange
+		//TODO: Implement UnitOfWorkTests
+
+		//Act
+
+
+		//Assert
+
+	}
+
+}
