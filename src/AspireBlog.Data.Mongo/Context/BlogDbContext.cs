23f07b8a6f21e127b6bf4225fa34e38022d4d435
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-12-22 12:05:51 -0800

    Add unique indexes to key columns in BlogDbContext
    Added unique indexes for `Id` and `Slug` properties across `Category`, `User`, and `BlogPost` entities to enforce uniqueness at the database level. This improves data integrity and ensures consistent lookups for key properties. Removed unnecessary code to simplify the context definition.

--- src/AspireBlog.Data.Mongo/Context/BlogDbContext.cs
+++ src/AspireBlog.Data.Mongo/Context/BlogDbContext.cs
@@ -1,6 +1,4 @@
-﻿using AspireBlog.Abstractions.Helpers;
-
-namespace AspireBlog.Data.Mongo.Context;
+﻿namespace AspireBlog.Data.Mongo.Context;
 
 public class BlogDbContext : DbContext
 {
@@ -8,146 +6,36 @@ public class BlogDbContext : DbContext
 	{
 	}
 
-	public static BlogDbContext Create(IMongoDatabase database) =>
-		new(new DbContextOptionsBuilder<BlogDbContext>()
-			.UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName)
-			.Options);
-
-	public DbSet<Category>? Categories { get; set; }
-	public DbSet<User>? Users { get; set; }
-	public DbSet<BlogPost>? BlogPosts { get; set; }
-
-	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-	{
-		base.OnConfiguring(optionsBuilder);
-#if DEBUG
-		optionsBuilder.LogTo(Console.WriteLine);
-#endif
-	}
+	public DbSet<Category> Categories { get; set; }
+	public DbSet<User> Users { get; set; }
+	public DbSet<BlogPost> BlogPosts { get; set; }
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
-		base.OnModelCreating(modelBuilder);
-
-		modelBuilder.Entity<User>()
-			.HasData(
-
-				new User
-				{
-
-					Id = ObjectId.GenerateNewId(),
-					Email = "matthew.paulosky@outlook.com",
-					FirstName = "Matthew",
-					LastName = "Paulosky",
-					FullName = "Matthew Paulosky",
-					Roles = ["Admin"]
-
-				}
-
-			);
-
-		modelBuilder.Entity<Category>()
-			.HasData(
-
-				new Category
-				{
-
-					Id = ObjectId.GenerateNewId(),
-					CategoryName = "ASP.NET Core",
-					Slug = Helpers.GetSlug("ASP.NET Core"),
-					IsArchived = false,
-					ArchivedBy = null
-
-				},
-				new Category
-				{
-
-					Id = ObjectId.GenerateNewId(),
-					CategoryName = "Blazor Server",
-					Slug = Helpers.GetSlug("Blazor Server"),
-					IsArchived = false,
-					ArchivedBy = null
-
-				}
-
-			);
-
+		// Add an index to the BlogPost slug column
 		modelBuilder.Entity<BlogPost>()
-			.HasData(
+			.HasIndex(p => p.Slug)
+			.IsUnique();
 
-				new BlogPost
-				{
+		// Add an index to the Category slug column
+		modelBuilder.Entity<Category>()
+			.HasIndex(p => p.Slug)
+			.IsUnique();
 
-					Id = ObjectId.GenerateNewId(),
-					Title = "First Blog Post",
-					Content = "This is the content of the first blog post.",
-					Slug = Helpers.GetSlug("First Blog Post"),
-					Introduction = "This is the introduction to the first blog post.",
-					CreatedOn = DateTime.UtcNow,
-					IsPublished = true,
-					PublishedOn = DateTime.UtcNow,
-					ModifiedOn = null,
-					Author = new User
-					{
+		// Add an index to the User Id column
+		modelBuilder.Entity<User>()
+			.HasIndex(p => p.Id)
+			.IsUnique();
 
-						Id = ObjectId.GenerateNewId(),
-						Email = "matthew.paulosky@outlook.com",
-						FirstName = "Matthew",
-						LastName = "Paulosky",
-						FullName = "Matthew Paulosky",
-						Roles = ["Admin"]
+		// Add an index to the BlogPost Id column
+		modelBuilder.Entity<BlogPost>()
+			.HasIndex(p => p.Id)
+			.IsUnique();
 
-					}.MapToUserDto(),
-					Category = new Category
-					{
-
-						Id = ObjectId.GenerateNewId(),
-						CategoryName = "ASP.NET Core",
-						Slug = Helpers.GetSlug("ASP.NET Core"),
-						IsArchived = false,
-						ArchivedBy = null
-
-					}.MapToCategoryDto()
-
-				},
-				new BlogPost
-				{
-
-					Id = ObjectId.GenerateNewId(),
-					Title = "Second Blog Post",
-					Content = "This is the content of the second blog post.",
-					Slug = Helpers.GetSlug("Second Blog Post"),
-					Introduction = "This is the introduction to the second blog post.",
-					CreatedOn = DateTime.UtcNow,
-					IsPublished = true,
-					PublishedOn = DateTime.UtcNow,
-					ModifiedOn = null,
-					Author = new User
-					{
-
-						Id = ObjectId.GenerateNewId(),
-						Email = "matthew.paulosky@outlook.com",
-						FirstName = "Matthew",
-						LastName = "Paulosky",
-						FullName = "Matthew Paulosky",
-						Roles = ["Admin"]
-
-					}.MapToUserDto(),
-					Category = new Category
-					{
-
-						Id = ObjectId.GenerateNewId(),
-						CategoryName = "Blazor Server",
-						Slug = Helpers.GetSlug("Blazor Server"),
-						IsArchived = false,
-						ArchivedBy = null
-
-					}.MapToCategoryDto()
-
-				}
-
-			);
-
+		// Add an index to the Category Id column
+		modelBuilder.Entity<Category>()
+			.HasIndex(p => p.Id)
+			.IsUnique();
+		base.OnModelCreating(modelBuilder);
 	}
-
 }
