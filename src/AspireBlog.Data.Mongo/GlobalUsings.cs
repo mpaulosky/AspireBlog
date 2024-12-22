5024542a352095fa0403e506f33ca7dc865cdcf5
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-12-22 12:15:42 -0800

    Refactor global usings in GlobalUsings.cs
    Streamlined global using statements by removing unnecessary empty lines and adding missing dependencies. Introduced a static import for ServiceNames to improve code clarity and readability.

--- src/AspireBlog.Data.Mongo/GlobalUsings.cs
+++ src/AspireBlog.Data.Mongo/GlobalUsings.cs
@@ -8,19 +8,17 @@
 // =============================================
 
 global using System.Linq.Expressions;
-
 global using Ardalis.GuardClauses;
-
 global using AspireBlog.Abstractions.Entities;
 global using AspireBlog.Abstractions.GuardClauses;
+global using AspireBlog.Abstractions.Helpers;
 global using AspireBlog.Abstractions.Interfaces;
 global using AspireBlog.Abstractions.Mappers;
 global using AspireBlog.Abstractions.Models;
 global using AspireBlog.Data.Mongo.Context;
 global using AspireBlog.Data.Mongo.Repositories;
-
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Logging;
-
 global using MongoDB.Bson;
 global using MongoDB.Driver;
+global using static AspireBlog.Abstractions.Constants.ServiceNames;
