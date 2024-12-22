c4790929cdbb871e8cac8955705a817cabdc8f25
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2024-12-22 12:14:24 -0800

    Add missing MongoDB.EntityFrameworkCore global using
    Moved and added global using statements to ensure required namespaces are included. This change organizes dependencies and resolves potential issues with missing or misplaced imports.

--- src/AspireBlog.Abstractions/GlobalUsings.cs
+++ src/AspireBlog.Abstractions/GlobalUsings.cs
@@ -4,15 +4,14 @@ global using System.Diagnostics.CodeAnalysis;
 global using Ardalis.GuardClauses;
 
 global using AspireBlog.Abstractions.Entities;
+global using AspireBlog.Abstractions.Enum;
 global using AspireBlog.Abstractions.GuardClauses;
 global using AspireBlog.Abstractions.Models;
-global using AspireBlog.Abstractions.Enum;
 
 global using Bogus;
 
-global using Microsoft.EntityFrameworkCore;
-
 global using MongoDB.Bson;
 global using MongoDB.Bson.Serialization.Attributes;
+global using MongoDB.EntityFrameworkCore;
 
 global using Category = AspireBlog.Abstractions.Entities.Category;
