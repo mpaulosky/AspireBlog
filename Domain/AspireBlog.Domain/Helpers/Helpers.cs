85fef63d9fb8e9a94348eae1669b74f6cb7f9d9c
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-01-15 05:36:48 -0800

    Add initial model, entity, fake, and helper classes
    This commit introduces foundational classes for the AspireBlog.Domain project. It includes models, DTOs, entities, enums, and various helpers to support domain logic. These files serve as a preliminary structure for future development.

---
+++ Domain/AspireBlog.Domain/Helpers/Helpers.cs
@@ -0,0 +1,17 @@
+// =======================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     Helpers.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : AspireBlog
+// Project Name :  AspireBlog.Domain
+// ========================================================
+
+namespace AspireBlog.Domain.Helpers;
+
+public class Helpers
+{
+
+	
+
+}
---
+++ Domain/AspireBlog.Domain/Models/UserInfo.cs
@@ -0,0 +1,17 @@
+// =======================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     UserInfo.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : AspireBlog
+// Project Name :  AspireBlog.Domain
+// ========================================================
+
+namespace AspireBlog.Domain.Models;
+
+public class UserInfo
+{
+
+	
+
+}
