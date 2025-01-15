849c45f8439360bc890e6ed9f7ef97d772fa8bf8
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-01-15 15:35:50 -0800

    Add repository interfaces to AspireBlog.Persistence
    Introduced `ICategoryRepository`, `IBlogPostRepository`, `IGenericRepository`, and `IUnitOfWork` interfaces to the project for better separation of concerns and scalability. Removed the unused "Interfaces" folder declaration from the project file.

---
+++ Persistance/AspireBlog.Persistence/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+// =======================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     IUnitOfWork.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : AspireBlog
+// Project Name :  AspireBlog.Persistence
+// ========================================================
+
+namespace AspireBlog.Persistence.Interfaces;
+
+public class IUnitOfWork
+{
+
+	
+
+}
