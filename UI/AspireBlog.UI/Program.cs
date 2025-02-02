5ee27efdb0fc5d2ec56e41c76073238eb263ac80
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-02-02 09:39:28 -0800

    Refactor code formatting and add MongoDB integration.
    The codebase has been reformatted consistently across multiple files for better readability and maintainability. Additionally, a MongoDB database context has been integrated, enabling the application to connect to a MongoDB instance using environment variables for configuration.

--- UI/AspireBlog.UI/Program.cs
+++ UI/AspireBlog.UI/Program.cs
@@ -1,6 +1,26 @@
-using AspireBlog.UI.Components;
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     Program.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : AspireBlog
+// Project Name :  AspireBlog.UI
+// =======================================================
 
+#region
+
+using AspireBlog.UI.Components;
+using AspireBlog.Domain.Constants;
 using Blazored.SessionStorage;
+
+#endregion
+
+using AspireBlog.Persistence;
+using AspireBlog.Persistence.Context;
+
+using Microsoft.EntityFrameworkCore;
+
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +32,23 @@ builder.Services.AddMemoryCache();
 
 builder.Services.AddBlazoredSessionStorage();
 
+builder.Services.AddDbContextFactory<BlogDbContext>(options =>
+{
+
+	var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI") ?? throw new InvalidOperationException("Environment variable 'MONGODB_URI' is not set.");
+	var databaseName = new ServiceNames().MongoDbName;
+	var mongoClient = new MongoClient(MongoClientSettings.FromConnectionString(connectionString));
+
+	options.UseMongoDB(mongoClient, databaseName);
+
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	app.UseExceptionHandler("/Error", true);
 
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
