f261c47c9b1928dff0795850650a61aa7fd16abb
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-02-02 09:53:45 -0800

    Add new global using directives and update file metadata
    Updated `GlobalUsings.cs` to include references to `AspireBlog.Domain.Fakes` and a `#region` block for better organization. Also added file metadata details, including author and project information, for improved documentation and clarity.

--- Persistance/AspireBlog.Persistence/GlobalUsings.cs
+++ Persistance/AspireBlog.Persistence/GlobalUsings.cs
@@ -1,3 +1,12 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : AspireBlog
+// Project Name :  AspireBlog.Persistence
+// =======================================================
+
 // =======================================
 // Copyright (c) 2025. All rights reserved.
 // File Name :     GlobalUsings.cs
@@ -7,11 +16,14 @@
 // Project Name :  AspireBlog.Persistence
 // ========================================================
 
+#region
+
 global using System.Linq.Expressions;
 
 global using Ardalis.GuardClauses;
 
 global using AspireBlog.Domain.Entities;
+global using AspireBlog.Domain.Fakes;
 global using AspireBlog.Domain.Helpers;
 global using AspireBlog.Domain.Models;
 global using AspireBlog.Persistence.Context;
@@ -20,3 +32,5 @@ global using AspireBlog.Persistence.Interfaces;
 global using Microsoft.EntityFrameworkCore;
 
 global using MongoDB.Bson;
+
+#endregion
